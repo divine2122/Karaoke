7fa6663161e4176191adbf1d5f9736a16b38db2c
Divine <divine2122@comcast.net>
2022-01-31 19:36:14 +0100

    user query search now hits backend for song lyrics

--- functions/index.js
+++ functions/index.js
@@ -28,9 +28,11 @@ const homeFunc = async (req, res) => {
   app.use(cors);
 
   const requestToken = req.query.code
-  const userInput = {songSearchKeywords:"doja cat juicy"}
+  //const userInput = {songSearchKeywords:"doja cat juicy"}
+  const userInput = {songSearchKeywords: req.query.state}
 
-console.log('hit backend', new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}))
+  
+console.log('hit backend',req.query, new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}))
 
 return await fetch(`https://api.genius.com/oauth/token?client_id=${clientId}&client_secret=${clientSecret}&code=${requestToken}&grant_type=${grant_type}&redirect_uri=${redirectUri}&response_type=${response_type}`, {
         method: 'post',
@@ -82,7 +84,7 @@ async function songMetadataGetter(userInput, accessToken){
    return fetch(songrequesturi, options)
    .then(async (res) =>  {
     var output = await res.json()
-    console.log('songMetadatagetter test', output.response.hits[0].result.primary_artist.image_url)
+    //console.log('songMetadatagetter test', output.response.hits[0].result.primary_artist.image_url)
     var songMetadata = output.response.hits[0].result
     return songMetadata
 
